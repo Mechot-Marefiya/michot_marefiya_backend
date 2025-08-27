32b6b5bc5730b7b364a95aa612bf9b1ff3956d19
Dagi <jegisew21@gmail.com>
2025-08-27 17:26:17 +0300

    feat: Added base model viewset

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+
+class AbstractModelViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
