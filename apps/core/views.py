e85d0b2dad2710e6aafdd6935f87ab3015e19418
Dagi <jegisew21@gmail.com>
2025-09-01 17:01:18 +0300

    Add some TODO comments

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,4 +2,4 @@ from rest_framework.viewsets import ModelViewSet
 
 
 class AbstractModelViewSet(ModelViewSet):
-    http_method_names = ['get', 'post', 'patch', 'delete']
+    http_method_names = ["get", "post", "patch", "delete"]
