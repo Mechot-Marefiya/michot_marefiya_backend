86a4a4058950917c577ac6c36dbbb1f57943b1aa
Dagi <jegisew21@gmail.com>
2025-09-01 16:59:19 +0300

    chore: added flags and installed a new app

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ THIRD_PARTY_APPS = [
     "corsheaders",
 ]
 
-CUSTOM_APPS = ["apps.account", "apps.core"]
+CUSTOM_APPS = ["apps.account", "apps.core", "apps.listing"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
@@ -132,6 +132,18 @@ USE_TZ = env("USE_TZ", cast=bool, default=True)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+# DATALOOKUP_MODEL = "core.DataLookup"
+
+# POLICIES_FILE_PATH = env(
+#     "ACCESS_POLICY_FILE",
+#     default=os.path.join(BASE_DIR, "config", "policies.json"),
+#     cast=str,
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
