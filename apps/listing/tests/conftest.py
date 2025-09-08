00fc1f2636c10c0de33e467ccda0f4fb252797f5
Dagi <jegisew21@gmail.com>
2025-09-08 21:43:25 +0300

    test: added integration tests

--- apps/listing/tests/conftest.py
+++ apps/listing/tests/conftest.py
@@ -1,17 +1,31 @@
+import json
+import pytest
 from io import BytesIO
 from PIL import Image
-import pytest
-from rest_framework.test import APIClient
-from apps.account.models import CompanyProfile, HotelProfile
 from django.core.files.uploadedfile import SimpleUploadedFile
-
+from rest_framework.test import APIClient
 from apps.core.models import Address
+from apps.account.models import (
+    CompanyProfile,
+    HotelProfile,
+    IndividualOwnerProfile
+)
+from apps.listing.models import Amenity
 
 
 @pytest.fixture
 def company_user(django_user_model):
     return django_user_model.objects.create_user(
         email="company@example.com", password="pass"
+    )
+
+
+@pytest.fixture
+def michot_admin(django_user_model, admin_role):
+    return django_user_model.objects.create_user(
+        email="company@example.com",
+        password="pass",
+        role=admin_role
     )
 
 
@@ -24,7 +38,32 @@ def create_test_image(name="test.png", ext="PNG", size=(100, 100), color=(255, 0
 
 
 @pytest.fixture
-def company_profile(company_user):
+def images_list():
+    imgs = []
+    for i in range(1, 4):
+        img_name = f"img_{i}.png"
+        imgs.append(create_test_image(img_name))
+    return imgs
+
+
+@pytest.fixture
+def address_payload():
+    return json.dumps(
+        {
+            "street_line1": "wollo sefer",
+            "country": "Ethiopia",
+            "city": "Addis Ababa",
+            "sub_city": "Bole",
+            "state": "Addis Ababa",
+            "postal_code": "1000",
+            "latitude": "45.12",
+            "longitude": "23.46",
+        }
+    )
+
+
+@pytest.fixture
+def company_profile(company_user) -> CompanyProfile:
     logo = create_test_image("logo.png")
     license = SimpleUploadedFile(
         "company_license.pdf", b"file_content", content_type="application/pdf"
@@ -37,36 +76,77 @@ def company_profile(company_user):
         "state": "Addis Ababa",
         "postal_code": "1000",
         "latitude": "45.12",
-        "longitude": "23.46",
+        "longitude": "23.46"
     }
 
     address = Address.objects.create(**address_data)
 
     data = {
-        "name": "michot",
+        "name": "some vendor",
         "phone": "+25111121214",
         "license": license,
         "address": address,
         "logo": logo,
-        "category": "hotel",
+        "category": CompanyProfile.CategoryChoice.HOTEL
     }
     return CompanyProfile.objects.create(user=company_user, **data)
 
 
 @pytest.fixture
-def hotel_profile(company_profile):
+def individual_owner_profile() -> IndividualOwnerProfile:
+    address_data = {
+        "street_line1": "22 mazoria",
+        "country": "Ethiopia",
+        "city": "Addis Ababa",
+        "sub_city": "Yeka",
+        "state": "Addis Ababa",
+        "postal_code": "1000",
+        "latitude": "9.03",
+        "longitude": "38.74",
+    }
+
+    address = Address.objects.create(**address_data)
+
+    data = {
+        "first_name": "Abebe",
+        "last_name": "Bekele",
+        "address": address,
+        "phone": "+251911223344",
+        "category": IndividualOwnerProfile.PropertyCategoryChoice.APARTMENT,
+        "national_id_number": 123456,
+    }
+    return IndividualOwnerProfile.objects.create(**data)
+
+
+@pytest.fixture
+def hotel_profile(company_profile) -> HotelProfile:
     return HotelProfile.objects.create(company=company_profile, stars=5)
 
 
 @pytest.fixture
-def authenticated_company_client(company_user, company_profile) -> APIClient:
+def authenticated_company_profile_client(company_user, company_profile) -> APIClient:
     client = APIClient()
     client.force_authenticate(user=company_user)
     return client
 
 
 @pytest.fixture
-def authenticated_hotel_profile_client(company_user, hotel_profile):
+def authenticated_michot_admin_client(michot_admin) -> APIClient:
+    client = APIClient()
+    client.force_authenticate(user=michot_admin)
+    return client
+
+
+@pytest.fixture
+def authenticated_hotel_profile_client(company_user, hotel_profile) -> APIClient:
     client = APIClient()
     client.force_authenticate(user=company_user)
     return client
+
+
+@pytest.fixture
+def amenities():
+    return [
+        Amenity.objects.create(name=am).id
+        for am in ["wifi", "pool", "balcony"]
+    ]
--- apps/listing/tests/test_listing.py
+++ apps/listing/tests/test_listing.py
@@ -1,11 +1,11 @@
-from io import BytesIO
+import pytest
 import json
 from PIL import Image
+from io import BytesIO
 from django.urls import reverse
-import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
-
-from apps.listing.models import Amenity, RoomListing
+from rest_framework import status
+from apps.listing.models import CarListing, RoomListing
 
 
 def create_test_image(name="test.png", ext="PNG", size=(100, 100), color=(255, 0, 0)):
@@ -16,99 +16,245 @@ def create_test_image(name="test.png", ext="PNG", size=(100, 100), color=(255, 0
     return SimpleUploadedFile(name, file.read(), content_type=f"image/{ext.lower()}")
 
 
-@pytest.mark.django_db
-def test_room_listing_creation(authenticated_hotel_profile_client):
-    images_list = []
-    for i in range(1, 6):
-        img_name = "img_" + str(i)
-        img = create_test_image(f"{img_name}.png")
-        images_list.append(img)
+@pytest.mark.django_db(transaction=True)
+class TestRoomListingAPI:
+    endpoint = "rooms-list"
 
-    address = json.dumps(
-        {
-            "street_line1": "wollo sefer",
-            "country": "Ethiopia",
-            "city": "Addis ababa",
-            "sub_city": "Bole",
-            "state": "Addis Ababa",
-            "postal_code": "1000",
-            "latitude": "45.12",
-            "longitude": "23.46",
+    def test_room_listing_creation(
+        self,
+        authenticated_hotel_profile_client,
+        address_payload,
+        images_list,
+        amenities
+    ):
+        print("AM", amenities)
+        data = {
+            "images": images_list,
+            "title": "Deluxe room",
+            "description": "A very luxury room with 2 king bed",
+            "base_price": "2000",
+            "address": address_payload,
+            "number_of_guests": 2,
+            "total_units": 10,
+            "amenities": amenities,
+            "bed_type": RoomListing.BedType.KING,
+            "room_size_sqm": 50,
+            "smoking_allowed": False,
+            "children_allowed": False
         }
-    )
 
-    amenities = [
-        Amenity.objects.create(name=am).id for am in ["wifi", "pool", "balcony"]
-    ]
+        res = authenticated_hotel_profile_client.post(
+            reverse(self.endpoint), data, format="multipart"
+        )
 
-    data = {
-        "images": images_list,
-        "title": "Deluxe room",
-        "description": "A very luxury room with 2 king bed",
-        "base_price": "2000",
-        "address": address,
-        "number_of_guests": 2,
-        "total_units": 10,
-        "amenities": amenities,
-        "bed_type": RoomListing.BedType.KING,
-        "room_size_sqm": 50,
-        "smoking_allowed": False,
-        "children_allowed": False
-    }
+        # print("RES", res.data)
 
-    res = authenticated_hotel_profile_client.post(
-        reverse("rooms-list"), data, format="multipart"
-    )
+        assert res.status_code == status.HTTP_201_CREATED
+        assert res.data["title"] == "Deluxe room"
+        assert res.data["bed_type"] == RoomListing.BedType.KING
 
-    print("RES", res.data)
+    # @pytest.mark.django_db
+    def test_room_listing_creation_fails_on_missed_data(
+        self,
+        authenticated_hotel_profile_client,
+        amenities,
+        address_payload,
+        images_list,
+    ):
 
-    assert res.status_code == 201
-    assert res.data["title"] == "Deluxe room"
-    assert res.data["bed_type"] == RoomListing.BedType.KING
-
-
-@pytest.mark.django_db
-def test_room_listing_creation_fails_on_missed_data(authenticated_hotel_profile_client):
-    images_list = []
-    for i in range(1, 6):
-        img_name = "img_" + str(i)
-        img = create_test_image(f"{img_name}.png")
-        images_list.append(img)
-
-    address = json.dumps(
-        {
-            "street_line1": "wollo sefer",
-            "country": "Ethiopia",
-            "city": "Addis ababa",
-            "sub_city": "Bole",
-            "state": "Addis Ababa",
-            "postal_code": "1000",
-            "latitude": "45.12",
-            "longitude": "23.46",
+        data = {
+            "images": images_list,
+            "title": "",
+            "base_price": "2000",
+            "address": address_payload,
+            "number_of_guests": 2,
+            "total_units": 10,
+            "amenities": amenities,
+            "bed_type": RoomListing.BedType.KING,
+            "room_size_sqm": 50,
         }
-    )
 
-    amenities = [
-        Amenity.objects.create(name=am).id for am in ["wifi", "pool", "balcony"]
-    ]
+        res = authenticated_hotel_profile_client.post(
+            reverse(self.endpoint), data, format="multipart"
+        )
 
-    data = {
-        "images": images_list,
-        "title": "",
-        "base_price": "2000",
-        "address": address,
-        "number_of_guests": 2,
-        "total_units": 10,
-        "amenities": amenities,
-        "bed_type": RoomListing.BedType.KING,
-        "room_size_sqm": 50
-    }
+        # print("RES", res.data)
 
-    res = authenticated_hotel_profile_client.post(
-        reverse("rooms-list"), data, format="multipart"
-    )
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
+        assert "This field may not be blank." in str(res.data)
 
-    print("RES", res.data)
 
-    assert res.status_code == 400
-    assert "This field may not be blank." in str(res.data)
+@pytest.mark.django_db(transaction=True)
+class TestGuestHouseListingAPI:
+    endpoint = 'guest_houses-list'
+
+    def test_create_guest_house_with_individual_owner(
+        self,
+        authenticated_client,
+        individual_owner_profile,
+        address_payload,
+        images_list,
+        amenities
+    ):
+
+        payload = {
+            'title': 'ABCD',
+            'base_price': 12500.00,
+            'total_rooms': 4,
+            'rating': 4,
+            'amenities': amenities,
+            'address': address_payload,
+            'individual_owner': individual_owner_profile.id,
+            'images': images_list
+        }
+
+        res = authenticated_client.post(
+            reverse(self.endpoint), payload, format='multipart'
+        )
+
+        print("RES", res.data)
+
+        assert res.status_code == status.HTTP_201_CREATED
+        assert res.data['total_rooms'] == 4
+        assert res.data['title'] == 'ABCD'
+
+    def test_create_guest_house_with_company_owner(
+        self,
+        authenticated_company_profile_client,
+        address_payload,
+        images_list,
+        amenities
+    ):
+
+        payload = {
+            'title': 'ABCD',
+            'base_price': 12500.00,
+            'total_rooms': 4,
+            'rating': 4,
+            'amenities': amenities,
+            'address': address_payload,
+            'images': images_list
+        }
+        res = authenticated_company_profile_client.post(
+            reverse(self.endpoint), payload, format='multipart'
+        )
+
+        print("RES", res.data)
+
+        assert res.status_code == status.HTTP_201_CREATED
+        assert res.data['total_rooms'] == 4
+        assert res.data['title'] == 'ABCD'
+
+    def test_create_guest_house_without_owner_should_fail(
+        self,
+        authenticated_michot_admin_client,
+        address_payload,
+        images_list,
+        amenities
+    ):
+        """Simulating if by any case the michot admin failed to send an individual owner profile.
+        Assume they're registering individually owner GuestHouse buy we missed that profile.
+        i.e: no individual or company owner.
+        """
+        payload = {
+            'title': 'ABCD',
+            'base_price': 12500.00,
+            'total_rooms': 4,
+            'rating': 4,
+            'amenities': amenities,
+            'address': address_payload,
+            'images': images_list
+        }
+
+        res = authenticated_michot_admin_client.post(
+            reverse(self.endpoint), payload, format='multipart'
+        )
+        assert res.status_code == 400
+        assert "Valid Company or individual owner must exist" in str(res.data)
+
+
+# @pytest.mark.django_db
+# class TestCarListingAPI:
+#     endpoint = reverse("cars-list")
+
+#     def test_create_car_with_company_owner(
+#         self,
+#         authenticated_company_profile_client,
+#         images_list,
+#     ):
+#         payload = {
+#             'title': 'Lamburgini',
+#             'base_price': 5_000_000,
+#             'images': images_list,
+#             "brand": CarListing.CarBrandChoices.BMW,
+#             "model": "X5",
+#             "year": 2020,
+#             "mileage": 25000,
+#             "fuel_type": CarListing.FuelTypeChoices.PETROL,
+#             "transmission": CarListing.TransmissionChoices.AUTOMATIC,
+#             "listing_type": CarListing.ListingTypeChoices.SELL
+#         }
+
+#         response = authenticated_company_profile_client.post(
+#             self.endpoint, payload, format="multipart"
+#         )
+
+#         assert response.status_code == status.HTTP_201_CREATED
+#         car = CarListing.objects.get(id=response.data["id"])
+#         assert car.company == authenticated_company_profile_client
+#         assert car.individual_owner is None
+#         assert str(car) == "bmw::X5"
+
+#     def test_create_car_with_individual_owner(
+#         self,
+#         authenticated_company_profile_client,
+#         images_list,
+#         individual_owner_profile
+#     ):
+#         payload = {
+#             'title': 'Corolla',
+#             'base_price': 5_000_000,
+#             'images': images_list,
+#             "individual_owner": individual_owner_profile.id,
+#             "brand": CarListing.CarBrandChoices.TOYOTA,
+#             "model": "Camry",
+#             "year": 2018,
+#             "mileage": 60000,
+#             "fuel_type": CarListing.FuelTypeChoices.HYBRID,
+#             "transmission": CarListing.TransmissionChoices.MANUAL,
+#             "listing_type": CarListing.ListingTypeChoices.SELL
+#         }
+
+#         response = authenticated_company_profile_client.post(
+#             self.endpoint, payload, format="json"
+#         )
+#         assert response.status_code == status.HTTP_201_CREATED
+#         car = CarListing.objects.get(id=response.data["id"])
+#         assert car.individual_owner == individual_owner_profile
+#         assert car.company is None
+
+#     def test_create_car_without_owner_should_fail(
+#             self,
+#             authenticated_company_profile_client
+#     ):
+#         payload = {
+#             "brand": CarListing.CarBrandChoices.AUDI,
+#             "model": "A4",
+#             "year": 2022,
+#             "mileage": 5000,
+#             "fuel_type": CarListing.FuelTypeChoices.DIESEL,
+#             "transmission": CarListing.TransmissionChoices.AUTOMATIC,
+#             "is_for_sale": True,
+#         }
+#         response = authenticated_company_profile_client.post(
+#             self.endpoint, payload, format="json"
+#         )
+#         assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+#     def test_retrieve_car_listings(
+#         self, authenticated_company_profile_client, car_listing_factory
+#     ):
+#         car = car_listing_factory()
+#         response = authenticated_company_profile_client.get(self.endpoint)
+#         assert response.status_code == status.HTTP_200_OK
+#         assert any(item["id"] == car.id for item in response.data)
