7c891de2c859bf20a3a558674b0047d6730da207
degisew mengist <jegisew21@gmail.com>
2025-11-06 01:42:14 +0300

    fix: add api prefix to URL

--- config/urls.py
+++ config/urls.py
@@ -31,7 +31,7 @@ from drf_spectacular.views import (
 from apps.account.views import CustomTokenObtainPairView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/v1/account/", include("apps.account.urls")),
     path("api/v1/core/", include("apps.core.urls")),
     path("api/v1/listing/", include("apps.listing.urls")),
